c2e1eb762ac7892577a279589d5726cf23178f22
Ian <ianbuen@gmail.com>
2022-12-16 16:14:13 +0800

    fix grids

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ window.onload = () => {
 window.onresize = () => {
     menu.width = window.innerWidth < 1080 ? "0px" : "auto";
     btnMenu.src = "./assets/images/icon-menu.svg";
+    btnMenu.style.position = "relative";
+    btnMenu.style.right = "0";
+    menu.height = '100%';
 }
 
 btnMenu.addEventListener('click', (e) => { 
@@ -21,11 +24,24 @@ btnMenu.addEventListener('click', (e) => {
 });
 
 const toggleMenu = () => {
-    if (menu.width == "0px") {
-        menu.width = '275px';
-        btnMenu.src = "./assets/images/icon-menu-close.svg";
-    } else {
-        menu.width = "0px";
-        btnMenu.src = "./assets/images/icon-menu.svg";
-    }  
+
+    if (menu.width == "0px") 
+        openMenu();
+    else
+        closeMenu();
 }; 
+
+const openMenu = () => {
+    menu.width = '275px';
+    menu.height = '100vh';
+    btnMenu.src = "./assets/images/icon-menu-close.svg";
+    btnMenu.style.position = "fixed";
+    btnMenu.style.right = "20px";
+}
+
+const closeMenu = () => {
+    menu.width = "0px";
+    btnMenu.src = "./assets/images/icon-menu.svg";
+    btnMenu.style.position = "relative";
+    btnMenu.style.right = "0";
+}
